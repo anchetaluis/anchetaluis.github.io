58064eb04467048e209926b6e6790b14f299f765
Luis Ancheta <lewisancheta@gmail.com>
2019-11-08 14:44:24 -0500

    update build

--- src/app-404.js
+++ src/app-404.js
@@ -1,7 +1,7 @@
 define(["../node_modules/@polymer/polymer/polymer-element.js", "./shared-styles.js"], function (_polymerElement, _sharedStyles) {
   "use strict";
 
-  var _templateObject_5862cc80025f11ea96187b7d46f3386f = /*#__PURE__*/ babelHelpers.taggedTemplateLiteral(["\n            <style include=\"shared-styles\">\n                :host {\n                  display: block;\n                  text-align: center;\n                }\n            </style>\n\n            <div class=\"main\">\n                <h1>Error: 404</h1>\n                <h2>Not Found</h2>\n          \n                <p>Oops! The requested page was not found. <a href=\"[[rootPath]]\">Head back to home.</a></p>\n            </div>\n    "]);
+  var _templateObject_24eb0740026011ea8642cfe18402105f = /*#__PURE__*/ babelHelpers.taggedTemplateLiteral(["\n            <style include=\"shared-styles\">\n                :host {\n                  display: block;\n                  text-align: center;\n                }\n            </style>\n\n            <div class=\"main\">\n                <h1>Error: 404</h1>\n                <h2>Not Found</h2>\n          \n                <p>Oops! The requested page was not found. <a href=\"[[rootPath]]\">Head back to home.</a></p>\n            </div>\n    "]);
 
   var App404 =
   /*#__PURE__*/
@@ -16,7 +16,7 @@ define(["../node_modules/@polymer/polymer/polymer-element.js", "./shared-styles.
     babelHelpers.createClass(App404, null, [{
       key: "template",
       get: function get() {
-        return (0, _polymerElement.html)(_templateObject_5862cc80025f11ea96187b7d46f3386f);
+        return (0, _polymerElement.html)(_templateObject_24eb0740026011ea8642cfe18402105f);
       }
     }]);
     return App404;
